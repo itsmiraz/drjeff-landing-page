c0f628ca5cfabfbc6e97c7fa79f6512feee06725
itsmiraz <mirajulislamofficial044@gmail.com>
2024-11-02 06:42:47 +0600

    new section added

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,18 +1,21 @@
-import Hero from '@/components/ResultPage/components/hero'
-import TestimonialSection from '@/components/ResultPage/components/testimonialSection'
-import TopBanner from '@/components/SciaticaLandingPage/components/topBanner'
-import { sourceSansPro } from '@/fonts/fonts'
-import React from 'react'
+import Hero from "@/components/ResultPage/components/hero";
+import TestimonialSection from "@/components/ResultPage/components/testimonialSection";
+import WhyTrustUs from "@/components/ResultPage/components/whyTrust";
+import TopBanner from "@/components/SciaticaLandingPage/components/topBanner";
+import { sourceSansPro } from "@/fonts/fonts";
+import React from "react";
 
 const Result = () => {
   return (
-    <main className={`min-h-screen mx-auto max-w-[2000px] bg-white text-[#1B365D] ${sourceSansPro.variable} font-sans`}>
-<TopBanner/>
-<Hero />
-<TestimonialSection/>
+    <main
+      className={`min-h-screen mx-auto max-w-[2000px] bg-white text-[#1B365D] ${sourceSansPro.variable} font-sans`}
+    >
+      <TopBanner />
+      <Hero />
+      <WhyTrustUs />
+      <TestimonialSection />
     </main>
+  );
+};
 
-  )
-}
-
-export default Result
+export default Result;
---
+++ src/components/ResultPage/components/whyTrust.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Develop from "@/assets/icons/Develop.svg";
+import Patient from "@/assets/icons/Patient.svg";
+import Drug from "@/assets/icons/Drug.svg";
+
+const WhyTrustUs = () => {
+  return (
+    <div className="bg-[#1B365D] pt-[50px] pb-[79px] ">
+      <h2 className="text-[40px] font-bold text-white text-center font-plantinMTpro font-serif">
+        Why trust our approach?
+      </h2>
+      <ul className="flex gap-x-[51px] pt-[45px] justify-center items-center">
+        <li className="flex   gap-x-[20px] justify-center items-center">
+          <Develop />
+          <h3 className="text-[20px] font-bold text-white  font-plantinMTpro leading-[24px] font-serif">Developed by  Dr. Jeff <br /> Garofalo, 22+ year spine <br /> expert.</h3>
+        </li>
+        <li className="flex  gap-x-[20px] justify-center items-center">
+          <Patient />
+          <h3 className="text-[20px] leading-[24px] font-bold text-white  font-plantinMTpro font-serif">Used successfully by <br /> thousands of patients.</h3>
+        </li>
+        <li className="flex  gap-x-[20px] justify-center items-center">
+          <Drug />
+          <h3 className="text-[20px] leading-[24px] font-bold  text-white font-plantinMTpro font-serif">Non-invasive, drug-free <br /> methods.</h3>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default WhyTrustUs;
