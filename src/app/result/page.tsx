51db9ce5e3a44df88bf588f2334fa3946bb728f6
itsmiraz <mirajulislamofficial044@gmail.com>
2024-11-03 09:42:05 +0600

    new change

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,8 +1,8 @@
-import Hero from "@/components/ResultPage/components/hero";
-import MeetDrJeff from "@/components/ResultPage/components/meetDrJeff";
-import Satisfaction from "@/components/ResultPage/components/satisfaction";
-import TestimonialSection from "@/components/ResultPage/components/testimonialSection";
-import WhyTrustUs from "@/components/ResultPage/components/whyTrust";
+import Hero from "@/components/ResultPage/hero";
+import MeetDrJeff from "@/components/ResultPage/meetDrJeff";
+import Satisfaction from "@/components/ResultPage/satisfaction";
+import TestimonialSection from "@/components/ResultPage/testimonialSection";
+import WhyTrustUs from "@/components/ResultPage/whyTrust";
 import Footer from "@/components/SciaticaLandingPage/components/footer";
 import TopBanner from "@/components/SciaticaLandingPage/components/topBanner";
 import { sourceSansPro } from "@/fonts/fonts";
